244f1d22339761f5e1917bcbea92467c6f5cd11d
angiekl74 <angiekl74@hotmail.com>
2020-03-29 11:24:17 -0700

    Fixed yaxis label issue. Added string otu value to integer array

--- docs/StarterCode/static/js/app.js
+++ docs/StarterCode/static/js/app.js
@@ -7,17 +7,14 @@ function getData() {
 
         var dataset = d3.select("#selDataset").node().value;
         var testCase = parseInt(dataset);
-        console.log(testCase);
+        // console.log(testCase);
         var results=[]
         var testId = data.metadata.map(row => row["id"])
         
-        // var ethnicity = data.metadata.map(row => row["ethnicity"])
-
         var resultindex = testId.indexOf(testCase, 0)
-        console.log("result",resultindex)
-
+        // console.log("result",resultindex)
         results.push(testId[resultindex])
-        console.log(results)  
+        // console.log(results)  
 
         finalResult = []
         var panel = d3.select("#sample-metadata");
@@ -27,7 +24,7 @@ function getData() {
             if (i === resultindex) {
                 Object.entries(data.metadata[i]).forEach(([key, value]) => 
                 finalResult.push(`${key}: ${value}`));
-                console.log("F", finalResult)
+                // console.log("F", finalResult)
                 for (var j = 0; j < finalResult.length; j++) {
                     var row = panel.append("p");
                     row.text(finalResult[j]);                
@@ -41,37 +38,39 @@ function getData() {
 
 getData();
 
-// Make the horizontal bar chart
+// Build horizontal bar chart
 function buildPlot(){
     d3.json("samples.json").then((importedData) => {
         var data = importedData;
 
         var dataset = d3.select("#selDataset").node().value;
         var testCase = parseInt(dataset);
-        console.log(testCase);
         var testId = data.metadata.map(row => row["id"])
         var resultindex = testId.indexOf(testCase, 0)
-        console.log("result in buildPlot",resultindex)
 
         var results=[]
         results.push(testId[resultindex])
-        console.log("Angie",results) 
-
+        
             var dataSamples = data.samples[resultindex].sample_values
-            console.log(dataSamples);
             var dataIds = data.samples[resultindex].otu_ids
             var dataLabels = data.samples[resultindex].otu_labels
-            console.log(dataIds);
+
             
             dataTop10Samples = dataSamples.slice(0,10)
             reversedataTop10Samples = dataTop10Samples.reverse()
             dataTop10IdValues = dataIds.slice(0,10)
             reversedataTop10Values = dataTop10IdValues.reverse().toString()
-            console.log(reversedataTop10Samples, dataTop10IdValues)
+
+            // Adding string "OTU" to integer array to fix label issues
+            var finalTop10IdValueArray = [];
+            dataTop10IdValues.forEach((i) => {
+                finalTop10IdValueArray.push("OTU: " + i)
+                })
+
 
             var trace = {
                 x: reversedataTop10Samples,
-                y: reversedataTop10Values,
+                y: finalTop10IdValueArray,
                 type: "bar",
                 text: dataLabels,
                 orientation: "h",
@@ -84,24 +83,16 @@ function buildPlot(){
             var data = [trace];
 
             var layout = {
-                title: `Test Id: ${testCase}`,
-                yaxis: {
-                    title: "OTU-IDS",
-                    tickmode: 'array',
-                    tickvals: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
-                    // ticktext: [dataSamples.forEach((i) => {console.log(i)})]
-                   }
-                    //still need to fix this so tick text dynamically changes
-            };   
+                title: `Test Subject Id #: ${testCase}`,
+                };
 
             Plotly.newPlot("bar", data, layout);    
             
-            buildBubble();
-        
+            buildBubble();        
     })
 }
 
-
+// Build bubble chart
 function buildBubble(){
     d3.json("samples.json").then((importedData) => {
         var data = importedData;
@@ -117,16 +108,14 @@ function buildBubble(){
         results.push(testId[resultindex])
         console.log("Angie",results) 
 
-            var dataSamples = data.samples[resultindex].sample_values
-            console.log("data samples", dataSamples);
-            var dataIds = data.samples[resultindex].otu_ids
-            console.log("data ids", dataIds);
-            var dataLabels = data.samples[resultindex].otu_labels
+            var dataSamples = data.samples[resultindex].sample_values;
+            var dataIds = data.samples[resultindex].otu_ids;
+            var dataLabels = data.samples[resultindex].otu_labels;
             console.log("data labels", dataLabels)
             
-            // dataTop10Samples = dataSamples
-            // dataTop10IdValues = dataIds
-            // console.log(dataTop10Samples, dataTop10IdValues)
+            dataTop10Samples = dataSamples
+            dataTop10IdValues = dataIds
+            console.log(dataTop10Samples, dataTop10IdValues)
 
             var trace = {
                 x: dataIds,
@@ -144,7 +133,7 @@ function buildBubble(){
             var data = [trace];
 
             var layout = {
-                title: `Bubble for Test Id: ${testCase}`,
+                title: `Test Subject Id #: ${testCase}`,
                 height: 600,
                 width: 1000                                         
             };   
